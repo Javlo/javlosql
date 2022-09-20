1abeaf2009b681bf4d9ec5c2fe00a43121f93989
VANDERMAESEN Patrick <patrick.vandermaesen@ext.europarl.europa.eu>
2022-09-20 09:00:09 +0200

    escape sql

--- src/main/java/org/javlo/helper/sql/SQLBuilder.java
+++ src/main/java/org/javlo/helper/sql/SQLBuilder.java
@@ -849,6 +849,73 @@ public class SQLBuilder {
 		st.close();
 	}
 	
+	public static String escapeString(String field, boolean escapeDoubleQuotes) {
+		StringBuilder sBuilder = new StringBuilder(field.length() * 11 / 10);
+
+		int stringLength = field.length();
+
+		for (int i = 0; i < stringLength; ++i) {
+			char c = field.charAt(i);
+
+			switch (c) {
+			case 0: /* Must be escaped for 'mysql' */
+				sBuilder.append('\\');
+				sBuilder.append('0');
+
+				break;
+
+			case '\n': /* Must be escaped for logs */
+				sBuilder.append('\\');
+				sBuilder.append('n');
+
+				break;
+
+			case '\r':
+				sBuilder.append('\\');
+				sBuilder.append('r');
+
+				break;
+
+			case '\\':
+				sBuilder.append('\\');
+				sBuilder.append('\\');
+
+				break;
+
+			case '\'':
+				sBuilder.append('\\');
+				sBuilder.append('\'');
+
+				break;
+
+			case '"': /* Better safe than sorry */
+				if (escapeDoubleQuotes) {
+					sBuilder.append('\\');
+				}
+
+				sBuilder.append('"');
+
+				break;
+
+			case '\032': /* This gives problems on Win32 */
+				sBuilder.append('\\');
+				sBuilder.append('Z');
+
+				break;
+
+			case '\u00a5':
+			case '\u20a9':
+				// escape characters interpreted as backslash by mysql
+				// fall through
+
+			default:
+				sBuilder.append(c);
+			}
+		}
+
+		return sBuilder.toString();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(">>>>>>>>> SQLBuilder.main : StringHelperSql.camelToSnake(clazz.getSimpleName()); = "+StringHelperSql.camelToSnake("MarketingDocument")); //TODO: remove debug trace
 	}
